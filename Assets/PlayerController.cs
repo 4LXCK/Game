d62ddf084b91581e7a113d6313aca5eec8731f62
cumo <cumo24386433@gmail.com>
2020-09-19 14:37:13 +0800

    add some player control

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 5.0f;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        MoveMent();
+    }
+
+    void MoveMent()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(new Vector3(0, 0, playerSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(new Vector3(0, 0, -playerSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(new Vector3(playerSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(new Vector3(-playerSpeed * Time.deltaTime, 0, 0));
+        }
+    }
+}
