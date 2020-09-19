f2b28b4bc178267787ef8f8be0e8b224c9c54a51
Sonaru_isuge <bread511206@gmail.com>
2020-09-19 14:40:31 +0800

    Player Movement
    加入Player  、 加入移動腳本 、 LeftShift加速

---
+++ Assets/PlayerControl/Script/PlayerMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float rotateSpeed;
+    public float walkSpeed;
+    public float runSpeed;
+    private float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+        RunSpeed();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * -moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0,-rotateSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        }
+    }
+
+    void RunSpeed()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = runSpeed;
+        }
+        else moveSpeed = walkSpeed;
+    }
+}
